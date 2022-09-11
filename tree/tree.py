785a484c7a5a9e2c7adbc105c328987c39fadbc6
FifiMelo <felipe336336@gmail.com>
2022-09-11 17:31:24 +0200

    added tree file

--- tree/tree.py
+++ tree/tree.py
@@ -1,59 +1,66 @@
 import chess 
 import random
-import copy
+import time
+
 
 
 class Counter:
     def __init__(self):
-        Counter.x = 0
+        Counter.number_of_positions = 0
 
 
 
 def static_evaluation_of_position(position):
-    Counter.x += 1
+    """
+    Fake evaluation function that draws number from (-3.0, 3.0),
+    but also increments Counter.number_of_positions
+    """
+    Counter.number_of_positions += 1
     return random.uniform(-3.0, 3.0)
 
 
 def children(position):
+    """
+    Function that yields all positions available from the given one.
+    """
     for move in position.legal_moves:
-        new_position = copy.copy(position)
-        new_position.push(move)
-        yield new_position
+        position.push(move)
+        yield position
+        position.pop()
 
 
 def minimax(position, depth, alpha, beta, maximizingPlayer):
-    if depth == 0:
-        print(position)
-        print("\n\n")
+    if depth == 0: #or checkmate, or stalemate etc.
         return static_evaluation_of_position(position)
-
-
     if maximizingPlayer:
-        maxEval = -1000000000
+        maxEval = -1000000
         for child in children(position):
             eval = minimax(child, depth - 1, alpha, beta, False)
             maxEval = max(maxEval, eval)
             alpha = max(alpha, eval)
             if beta <= alpha:
+                position.pop()
                 break
         return maxEval
     else:
-        minEval = 1000000000
+        minEval = 1000000
         for child in children(position):
             eval = minimax(child, depth - 1, alpha, beta,  True)
             minEval = min(minEval, eval)
             beta = min(beta, eval)
             if beta <= alpha:
+                position.pop()
                 break
         return minEval
 
 def main():
-    a = Counter()
+    Counter()
+    depth = 6
     current_position = chess.Board(fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-    minimax(current_position, 2, -1000000000, 1000000000, True)
-    print(Counter.x)
-    
-    pass
+    start_time = time.time()
+    minimax(current_position, depth, -1000000, 1000000, True)  
+    print(f"On depth {depth} it took {time.time() - start_time}s to statically evaluate {Counter.number_of_positions} positions.")
+    print(f"Without move ordering and without transposition table.")
 
 if __name__ == "__main__":
     main()
