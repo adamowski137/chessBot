bbab48c6515e4a24485dc98a207d3890b058a952
szewczyk.franciszek02 <szewczyk.franciszek02@gmail.com>
2022-09-19 21:45:19 +0200

    Automated moves, Tree class.

--- main.py
+++ main.py
@@ -4,22 +4,33 @@ from ui.ui import Board
 import argparse
 import math
 
+
 def main(args):
-    board = Board(fen = args.fen)
+    board = Board(fen=args.fen)
+    tree = Tree(depth=args.depth)
 
     if args.display:
         board.display()
 
-    minimax(board, 5, -math.inf, math.inf, True)
-    
-        
+    for i in range(100):
+        eval, move = tree.minimax(board)
+
+        if move is None:
+            break
+
+        print(eval, move)
+        board.push(move)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Chess bot")
 
     # Values
-    parser.add_argument('-f', '--fen', help="Name of the input XML file.", default='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', required=False)
-    parser.add_argument('-d', '--depth', help="Maximum search depth.", default=3, type=float, required=False)
+    parser.add_argument('-f', '--fen', help="Name of the input XML file.",
+                        default='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', required=False)
+    parser.add_argument('-d', '--depth', help="Maximum search depth.",
+                        default=6, type=float, required=False)
 
     # Flags
     parser.add_argument('--display', dest="display",
@@ -27,11 +38,7 @@ if __name__ == "__main__":
 
     # Default flags
     parser.set_defaults(display=False)
-    
+
     args = parser.parse_args()
 
     main(args)
-
-    
-
-
--- tree/tree.py
+++ tree/tree.py
@@ -1,54 +1,90 @@
-import chess 
+import chess
 import random
 import time
-
-number_of_positions = 0
-
-
-def static_evaluation_of_position(position):
-    figures = position.piece_map()
-
-    pieces = {"p": -1, "P": 1, "n": -3, "N": 3, "b": -3, "B":3, "r": -5, "R":5, "q":-9, "Q":9, "k": -0, "K": 0 }
-    sum = 0
-    for i, value in figures.items():
-        sum += pieces[value.symbol()]
-    return sum
-        
-
-def children(position):
-    """
-    Function that yields all positions available from the given one.
-    """
-    for move in position.legal_moves:
-        position.push(move)
-        yield position
-        position.pop()
+import math
 
 
-def minimax(position, depth, alpha, beta, maximizingPlayer):
-    if depth == 0: #or checkmate, or stalemate etc.
-        eval = static_evaluation_of_position(position)
-        if abs(eval) > 5:
-            print(f"{position.fen()}, --- {eval}")
-        return eval
-    if maximizingPlayer:
-        maxEval = -1000000
-        for child in children(position):
-            eval = minimax(child, depth - 1, alpha, beta, False)
-            maxEval = max(maxEval, eval)
-            alpha = max(alpha, eval)
-            if beta <= alpha:
-                position.pop()
-                break
-        return maxEval
-    else:
-        minEval = 1000000
-        for child in children(position):
-            eval = minimax(child, depth - 1, alpha, beta,  True)
-            minEval = min(minEval, eval)
-            beta = min(beta, eval)
-            if beta <= alpha:
-                position.pop()
-                break
-        return minEval
+class Tree:
+    def __init__(self, depth):
+        self.depth = depth
+        self.best_move = None
+        self.eval = 0
+        self.positions = 0
 
+    def static_evaluation_of_board(self, board, depth):
+        outcome = board.outcome()
+        if outcome:
+            if outcome.termination in [chess.Termination.STALEMATE, chess.Termination.INSUFFICIENT_MATERIAL, chess.Termination.FIVEFOLD_REPETITION, chess.Termination.SEVENTYFIVE_MOVES, chess.Termination.THREEFOLD_REPETITION]:
+                return 0
+            if outcome.termination is chess.Termination.CHECKMATE and outcome.winner is True:
+                return 100000 + depth
+            elif outcome.termination is chess.Termination.CHECKMATE and outcome.winner is False:
+                return -100000 - depth
+
+        figures = board.piece_map()
+        pieces = {"p": -1, "P": 1, "n": -3, "N": 3, "b": -3, "B": 3,
+                  "r": -5, "R": 5, "q": -9, "Q": 9, "k": -0, "K": 0}
+        sum = 0
+        for i, value in figures.items():
+            sum += pieces[value.symbol()]
+        return sum
+
+    def minimax(self, board):
+        self.best_move = None
+        self.positions = 0
+        self.eval = self.__minimax(board, self.depth, -
+                                   math.inf, math.inf, board.turn)
+
+        return self.eval, self.best_move
+
+    def __children(self, board):
+        """
+        Function that yields all boards available from the given one.
+        """
+        for move in board.legal_moves:
+            try:
+                board.push(move)
+            except:
+                print(move)
+            yield board
+            board.pop()
+
+    def __minimax(self, board, depth, alpha, beta, maximizingPlayer):
+        if depth == 0 or board.outcome():  # or checkmate, or stalemate etc.
+            eval = self.static_evaluation_of_board(board, depth)
+            return eval
+
+        if maximizingPlayer:
+            maxEval = -1000000
+            for child in self.__children(board):
+                eval = self.__minimax(child, depth -
+                                      1, alpha, beta, False)
+
+                if eval >= maxEval:
+                    maxEval = eval
+                    if depth == self.depth:
+                        self.best_move = board.peek()
+
+                alpha = max(alpha, eval)
+                if beta <= alpha:
+                    board.pop()
+                    break
+
+            return maxEval
+        else:
+            minEval = 1000000
+            for child in self.__children(board):
+                eval = self.__minimax(child, depth -
+                                      1, alpha, beta,  True)
+
+                if eval <= minEval:
+                    minEval = eval
+                    if depth == self.depth:
+                        self.best_move = board.peek()
+
+                beta = min(beta, eval)
+                if beta <= alpha:
+                    board.pop()
+                    break
+
+            return minEval
